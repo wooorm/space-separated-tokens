b9b872442f1ba271c5fd04a23da49e2de08cf44f
Titus Wormer <tituswormer@gmail.com>
2016-11-24 19:07:43 +0100

    Refactor code-style

--- index.js
+++ index.js
@@ -1,73 +1,24 @@
-/**
- * @author Titus Wormer
- * @copyright 2016 Titus Wormer
- * @license MIT
- * @module space-separated-tokens
- * @fileoverview Parse and stringify space-separated tokens.
- */
-
 'use strict';
-
-/* eslint-env commonjs */
-
-/*
- * Dependencies.
- */
 
 var trim = require('trim');
 
-/*
- * Constants.
- */
+exports.parse = parse;
+exports.stringify = stringify;
 
-var EMPTY = '';
+var empty = '';
+var space = ' ';
+var whiteSpace = /[ \t\n\r\f]+/g;
 
-/*
- * Characters.
- */
-
-var C_SPACE = ' ';
-
-/*
- * Expressions.
- *
- * HTML white-space is slightly different from JavaScript’s
- * `\s`, as the latter includes vertical tabs.
- */
-
-var RE_WHITE_SPACE = /[\ \t\n\r\f]+/g;
-
-/**
- * Parse space-separated tokens to an array.
- *
- * @param {string} value - Attribute-value to parse.
- * @return {Array.<string>} - Tokens.
- */
 function parse(value) {
-    var input = trim(String(value || EMPTY));
+  var input = trim(String(value || empty));
 
-    if (input === EMPTY) {
-        return [];
-    }
+  if (input === empty) {
+    return [];
+  }
 
-    return input.split(RE_WHITE_SPACE);
+  return input.split(whiteSpace);
 }
 
-/**
- * Compile an array to space-separated tokens.
- *
- * @param {Array.<string>} values - Tokens.
- * @return {string} - Space-separated tokens.
- */
 function stringify(values) {
-    return trim(values.join(C_SPACE));
+  return trim(values.join(space));
 }
-
-/*
- * Expose.
- */
-
-module.exports = {
-    'parse': parse,
-    'stringify': stringify
-};

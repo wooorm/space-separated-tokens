b58741f37175ca9eaf9abe49256607dfb81ee39a
Titus Wormer <tituswormer@gmail.com>
2021-03-07 20:21:25 +0100

    Add JSDoc based types

--- test.js
+++ test.js
@@ -5,6 +5,7 @@ test('space-separated-tokens', function (t) {
   t.test('.parse()', function (st) {
     st.equal(typeof parse, 'function', 'should be a method')
 
+    // @ts-ignore runtime
     st.deepEqual(parse(), [], 'should return an empty array for an empty value')
 
     st.deepEqual(
